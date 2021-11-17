9a69cb31631198c90976b88e6f9e89ec274341d4
Åsa Berglund <you@example.com>
2021-11-17 10:57:29 +0100

    g kriterier klart

--- Program.cs
+++ Program.cs
@@ -11,39 +11,46 @@ namespace Posts
     public class GuestBook
     {
         private List<Post> posts = new List<Post>();
-        public GuestBook(){ 
-            if(File.Exists(@"guestbook.data")==true){
+        public GuestBook()
+        {
+            if (File.Exists(@"guestbook.data") == true)
+            {
                 IFormatter formatter = new BinaryFormatter();
-                Stream stream = new FileStream(@"guestbook.data",FileMode.Open,FileAccess.Read);
+                Stream stream = new FileStream(@"guestbook.data", FileMode.Open, FileAccess.Read);
 
-                while(stream.Position<stream.Length){   
+                while (stream.Position < stream.Length)
+                {
                     Post objnew = (Post)formatter.Deserialize(stream);
                     posts.Add(objnew);
                 }
                 stream.Close();
             }
         }
-        public Post addPost(Post post){
+        public Post addPost(Post post)
+        {
             posts.Add(post);
-            marshall();         
+            marshall();
             return post;
         }
-        
-        public int delPost(int index){
+
+        public int delPost(int index)
+        {
             posts.RemoveAt(index);
             marshall();
             return index;
         }
 
-        public List<Post> getPosts(){
+        public List<Post> getPosts()
+        {
             return posts;
         }
 
         private void marshall()
         {
             IFormatter formatter = new BinaryFormatter();
-            Stream stream = new FileStream(@"guestbook.data",FileMode.Create,FileAccess.Write);
-            foreach(Post obj in posts){
+            Stream stream = new FileStream(@"guestbook.data", FileMode.Create, FileAccess.Write);
+            foreach (Post obj in posts)
+            {
                 formatter.Serialize(stream, obj);
             }
             stream.Close();
@@ -53,11 +60,17 @@ namespace Posts
     [Serializable]
     public class Post
     {
-        private string author;        
+        private string author;
+        private string text;
         public string Author
         {
-            set {this.author = value;}
-            get {return this.author;}
+            set { this.author = value; }
+            get { return this.author; }
+        }
+        public string Text
+        {
+            set { this.text = value; }
+            get { return this.text; }
         }
     }
 
@@ -65,44 +78,50 @@ namespace Posts
     {
         static void Main(string[] args)
         {
- 
-            GuestBook guestbook = new GuestBook();
-            int i=0;
 
-            while(true){
-                Console.Clear();Console.CursorVisible = false;
+            GuestBook guestbook = new GuestBook();
+            int i = 0;
+
+            while (true)
+            {
+                Console.Clear(); Console.CursorVisible = false;
                 Console.WriteLine("Gästbok\n\n");
 
                 Console.WriteLine("1. Lägg till inlägg");
                 Console.WriteLine("2. Ta bort inlägg\n");
                 Console.WriteLine("X. Avsluta\n");
 
-                i=0;
-                foreach(Post post in guestbook.getPosts()){
-                    Console.WriteLine("[" + i++ + "] " + post.Author);
+                i = 0;
+                foreach (Post post in guestbook.getPosts())
+                {
+                    Console.WriteLine("[" + i++ + "] " + post.Author + "\n\n" + post.Text );
                 }
 
                 string inp = Console.ReadLine().ToLower();
-                switch (inp) {
+                switch (inp)
+                {
                     case "1":
-                        Console.CursorVisible = true; 
+                        Console.CursorVisible = true;
                         Console.Write("Ange namn: ");
                         string author = Console.ReadLine();
+                        Console.Write("Skriv inlägg: ");
+                        string text = Console.ReadLine();
                         Post obj = new Post();
                         obj.Author = author;
+                        obj.Text = text;
                         guestbook.addPost(obj);
                         break;
-                    case "2": 
+                    case "2":
                         Console.CursorVisible = true;
                         Console.Write("Ange index att radera: ");
                         string index = Console.ReadLine();
                         guestbook.delPost(Convert.ToInt32(index));
                         break;
-                    case "x": 
+                    case "x":
                         Environment.Exit(0);
                         break;
                 }
- 
+
             }
 
         }
