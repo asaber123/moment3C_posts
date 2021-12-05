e7700d7a47eda5dc3c1fcc2829487f7e5483d53d
Åsa Berglund <you@example.com>
2021-12-05 09:49:02 +0100

    new version nothing works

--- Program.cs
+++ Program.cs
@@ -1,111 +1,109 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-
-using System.Runtime.Serialization;
-using System.Runtime.Serialization.Formatters.Binary;
+using System.Text.Json;
+//using System.Runtime.Serialization;
+//using System.Runtime.Serialization.Formatters.Binary;
 
 
 namespace Posts
 {
-    public class GuestBook
-    {
-        private List<Post> posts = new List<Post>();
-        public GuestBook()
-        {
-            if (File.Exists(@"guestbook.data") == true)
-            {
-                IFormatter formatter = new BinaryFormatter();
-                Stream stream = new FileStream(@"guestbook.data", FileMode.Open, FileAccess.Read);
-
-                while (stream.Position < stream.Length)
-                {
-                    Post objnew = (Post)formatter.Deserialize(stream);
-                    posts.Add(objnew);
-                }
-                stream.Close();
-            }
-        }
-        public Post addPost(Post post)
-        {
-            posts.Add(post);
-            marshall();
-            return post;
-        }
-
-        public int delPost(int index)
-        {
-            posts.RemoveAt(index);
-            marshall();
-            return index;
-        }
-
-        public List<Post> getPosts()
-        {
-            return posts;
-        }
-
-        private void marshall()
-        {
-            IFormatter formatter = new BinaryFormatter();
-            Stream stream = new FileStream(@"guestbook.data", FileMode.Create, FileAccess.Write);
-            foreach (Post obj in posts)
-            {
-                formatter.Serialize(stream, obj);
-            }
-            stream.Close();
-        }
-    }
-
-    [Serializable]
     public class Post
     {
         private string author;
-        private string text;
         public string Author
         {
-            set
-            {
-                this.author = value;
-
-            }
+            set { this.author = value; }
             get { return this.author; }
         }
+        private string text;
         public string Text
         {
             set { this.text = value; }
             get { return this.text; }
         }
     }
+    //Making a class for the posts in the gustbook. 
+    public class GuestBook
+    {   //Making a list to stora data
 
+        private List<Post> posts = new List<Post>();
+        private string filename = @"guestbook.json";
+        //Constructor 
+        public GuestBook(){
+            //Checking if file exist
+            if (File.Exists(@"guestbook.json") == true)
+            {
+                //Serialise and desirialise data to json format. 
+                string jsonString = File.ReadAllText(filename);
+                posts= JsonSerializer.Deserialize<List<Post>>(jsonString);
+            }
+        }
+        //Function to add a new post
+        public Post addPost(string auth, string text)
+        {
+            Post obj = new Post();
+            obj.Author= auth;
+            obj.Text = text;
+            posts.Add(obj);
+            //Using marshall to serilaise the data into json format. 
+            marshall();
+            return obj;
+        }
+        //Function to delete a post with an index
+        public int delPost(int index)
+        {
+            posts.RemoveAt(index);
+            marshall();
+            return index;
+        }
+        //Function to get all posts through calling the list. 
+        public List<Post> getPosts()
+        {
+            return posts;
+        }
+        //Function marshall which serialisase the list of posts
+        private void marshall()
+        {
+            //Serialising the post to json format and save it into the list
+            var jsonString = JsonSerializer.Serialize(posts);
+            File.WriteAllText(filename,jsonString);
+
+        }
+    }
+    //Main program
     class Program
     {
         static void Main(string[] args)
         {
-
+            //Making an instance of the guestbook where al public methods can be used
             GuestBook guestbook = new GuestBook();
             int i = 0;
 
             while (true)
             {
+                //clear the console and shut down cursor
                 Console.Clear(); Console.CursorVisible = false;
                 Console.WriteLine("Gästbok\n\n");
 
+                //Menu option
                 Console.WriteLine("1. Lägg till inlägg");
                 Console.WriteLine("2. Ta bort inlägg\n");
-                Console.WriteLine("X. Avsluta\n");
+                Console.WriteLine("3. Avsluta\n");
 
+                //Printing out all posts and there index in the guestbook
                 i = 0;
                 foreach (Post post in guestbook.getPosts())
                 {
-                    Console.WriteLine("[" + i++ + "] " + post.Author + "\n" + post.Text + "\n\n");
+                    Console.WriteLine("[" + i++ + "] " + post.Author + 
+                    // "\n" + post.Text + 
+                    "\n\n");
                 }
-
-                string inp = Console.ReadLine().ToLower();
-                switch (inp)
+                //Reading the users input. The function readkey reacs without user pressing enter. 
+                int input = (int) Console.ReadKey(true).Key;
+                switch (input)
                 {
-                    case "1":
-
+                    case '1':
                         Console.CursorVisible = true;
                         Post obj = new Post();
                         string author;
@@ -121,7 +119,6 @@ namespace Posts
                             }
 
                         } while (String.IsNullOrEmpty(author));
-                        obj.Author = author;
                         do
                         {
 
@@ -132,18 +129,16 @@ namespace Posts
                                 Console.Write("Du måste skriva någon text för ditt inlägg\n");
 
                             }
-                        }while (String.IsNullOrEmpty(text));
-                        obj.Text = text;
-                        guestbook.addPost(obj);
-
+                        } while (String.IsNullOrEmpty(text));
+                        guestbook.addPost(author, text);
                         break;
-                    case "2":
+                    case 2:
                         Console.CursorVisible = true;
                         Console.Write("Ange index att radera: ");
                         string index = Console.ReadLine();
                         guestbook.delPost(Convert.ToInt32(index));
                         break;
-                    case "x":
+                    case 3:
                         Environment.Exit(0);
                         break;
                 }
